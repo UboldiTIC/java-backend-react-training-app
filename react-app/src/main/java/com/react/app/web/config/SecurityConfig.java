c1c6da6346ff50e138f596e584eba281deb0416d
Solmar <solmaruboldi@gmail.com>
2025-10-17 20:50:05 -0300

    Disable csrf

---
+++ react-app/src/main/java/com/react/app/web/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.react.app.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /*
+        //Métodos deprecados:
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        */
+        http
+                .authorizeHttpRequests(customizeRequests -> {
+                    customizeRequests
+                            .anyRequest()
+                            .authenticated();
+                    }
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
