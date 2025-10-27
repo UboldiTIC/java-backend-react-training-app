43bb3cee20156f6f904eb057adaed33b20c0a817
Solmar <solmaruboldi@gmail.com>
2025-10-27 12:48:37 -0300

    Add JwtUtil

---
+++ react-app/src/main/java/com/react/app/web/config/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.react.app.web.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JwtUtil {
+    private static String SECRET_KEY = "2ol_m4r";
+    private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
+    public String create(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("admin")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                .sign(ALGORITHM);
+    }
+}
