9f884f231aeda69287b384e8a8b3ce1369fc8fd9
Solmar <solmaruboldi@gmail.com>
2025-10-28 14:08:43 -0300

    Create AuditUsername and edit AuditableEntity

--- react-app/src/main/java/com/react/app/persistence/audit/AuditableEntity.java
+++ react-app/src/main/java/com/react/app/persistence/audit/AuditableEntity.java
@@ -2,7 +2,9 @@ package com.react.app.persistence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -18,4 +20,11 @@ public class AuditableEntity {
     @LastModifiedDate
     private LocalDateTime modifiedDate;
 
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    @LastModifiedBy
+    private String modifiedBy;
 }
